b98cd62181eb71d616aec885752ea93ece0e9229
Lucas Lannes <lucas.lannes123@gmail.com>
2023-08-11 12:04:43 -0300

    Add functionality to persist data from user's pag

--- luggfinder/luggfinder/routes/users.py
+++ luggfinder/luggfinder/routes/users.py
@@ -1,5 +1,5 @@
 from luggfinder.db_models import Process, db
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 
 user_bp = Blueprint('user', __name__)
@@ -8,9 +8,30 @@ user_bp = Blueprint('user', __name__)
 @user_bp.route('/user')
 def home():
     query = Process.query.all()
-    suppliers = ["", "GvExpress", "Três Company", "Voucher"]
-
+    
     return render_template("user_pages/user.html", 
                             title="User", 
-                            query=query,
-                            suppliers=suppliers)
+                            query=query)
+
+
+@user_bp.route('/data_from_user', methods=['POST'])
+def data_from_user():
+    json_data = request.get_json()
+
+    for i in json_data:
+        update_process = Process.query.filter_by(process=json_data[i]["process"]).first();
+
+        update_process.date = json_data[i]["date"]
+        update_process.name = json_data[i]["name"]
+        update_process.pnr = json_data[i]["pnr"]
+        update_process.process_type = json_data[i]["process_type"]
+        update_process.fault_station = json_data[i]["fault_station"]
+        update_process.process_reason = json_data[i]["process_reason"]
+        update_process.cost = json_data[i]["cost"]
+        update_process.supplier = json_data[i]["supplier"]
+
+        db.session.commit()
+
+    Process.query.all()
+
+    return "received"
